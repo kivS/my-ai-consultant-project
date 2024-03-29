84a848a24fa249ccfc427da574d72651c24cb7fe
Vik Borges <v1k@protonmail.com>
2024-03-29 09:42:14 +0800

    🚀

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
@@ -59,6 +60,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </div>
           <TailwindIndicator />
           <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
