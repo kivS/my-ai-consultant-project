284d02f701b15a83cc595716ee4e07e62d63dbc2
Vik Borges <v1k@protonmail.com>
2024-05-18 15:28:00 +0800

    🚀

--- frontend/src/lib/chat/actions.jsx
+++ frontend/src/lib/chat/actions.jsx
@@ -1,16 +1,11 @@
-import 'server-only'
+import "server-only";
 
-import { OpenAI } from "openai";
-import { createAI, getMutableAIState, render } from "ai/rsc";
+import { createAI, getMutableAIState, streamUI } from "ai/rsc";
 import { z } from "zod";
 import { SpinnerMessage } from "@/components/chat/message";
-import Whiteboard from '@/components/whiteboard/whiteboard';
-import DatabaseWhiteboard from '@/components/database-whiteboard';
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
+import Whiteboard from "@/components/whiteboard/whiteboard";
+import DatabaseWhiteboard from "@/components/database-whiteboard";
+import { openai } from "@ai-sdk/openai";
 
 const system_root_prompt = `\
 // You are a database architect conversation bot and you can help users model their database architecture, step by step.
@@ -20,123 +15,135 @@ const system_root_prompt = `\
 // by calling to show the user by calling  \`update_database_whiteboard\`, here
 // you show current state of the database discused with the user: the tables, relationships, etc.
 
-// Besides that, you can also chat with users and do some calculations if needed.`
-
-
+// Besides that, you can also chat with users and do some calculations if needed.`;
 
 // An example of a spinner component. You can also import your own components,
 // or 3rd party component libraries.
 
-
 function Spinner() {
-  return <div>Loading...</div>;
+	return <div>Loading...</div>;
 }
-
 
 // An example of a function that fetches flight information from an external API.
 async function getFlightInfo(flightNumber) {
-  return {
-    flightNumber,
-    departure: 'New York',
-    arrival: 'San Francisco',
-  };
+	return {
+		flightNumber,
+		departure: "New York",
+		arrival: "San Francisco",
+	};
 }
 
 async function submitUserMessage(userInput) {
-  'use server';
+	"use server";
 
-  /**
-   * Json context for the LLM
-   */
-  const aiState = getMutableAIState();
+	/**
+	 * Json context for the LLM
+	 */
+	const aiState = getMutableAIState();
 
-  // Update the AI state with the new user message.
-  aiState.update([
-    ...aiState.get(),
-    {
-      role: 'user',
-      content: userInput,
-    },
-  ]);
+	// Update the AI state with the new user message.
+	aiState.update([
+		...aiState.get(),
+		{
+			role: "user",
+			content: userInput,
+		},
+	]);
 
-  // The `render()` creates a generated, streamable UI.
-  const ui = render({
-    // model: 'gpt-4-0125-preview',
-    model: 'gpt-3.5-turbo',
-    provider: openai,
-    initial: <SpinnerMessage />,
-    messages: [
-      { role: 'system', content: system_root_prompt },
-      ...aiState.get()
-    ],
-    // `text` is called when an AI returns a text response (as opposed to a tool call).
-    // Its content is streamed from the LLM, so this function will be called
-    // multiple times with `content` being incremental.
-    text: ({ content, done }) => {
-      // When it's the final content, mark the state as done and ready for the client to access.
-      if (done) {
-        aiState.done([
-          ...aiState.get(),
-          {
-            role: "assistant",
-            content
-          }
-        ]);
-      }
+	// The `render()` creates a generated, streamable UI.
+	const result = await streamUI({
+		model: openai("gpt-3.5-turbo"),
+		initial: <SpinnerMessage />,
+		// system: system_root_prompt,
+		messages: [
+			{ role: "system", content: system_root_prompt },
+			...aiState.get(),
+		],
+		// `text` is called when an AI returns a text response (as opposed to a tool call).
+		// Its content is streamed from the LLM, so this function will be called
+		// multiple times with `content` being incremental.
+		text: ({ content, done }) => {
+			// When it's the final content, mark the state as done and ready for the client to access.
+			if (done) {
+				aiState.done([
+					...aiState.get(),
+					{
+						role: "assistant",
+						content,
+					},
+				]);
+			}
 
-      return <p>{content}</p>
-    },
-    tools: {
-      update_database_whiteboard: {
-        description: 'Update the whiteboard for the database modeling. it generates the current state of the database based on the conversation context ',
-        parameters: z.object({
-          initialNodes: z.array(
-            z.object({
-              id: z.string().describe('ID for the node, representing the table, eg: table_name'),
-              position: z.object({
-                x: z.number().describe('the position of the node in the x axis'),
-                y: z.number().describe('the position of the node in the y axis')
-              }),
-              data: z.object({
-                label: z.string().describe('the name of the table')
-              })
-            })
-          ).describe('an array of nodes definition for reactflow that\'ll represent the current state of the database')
-        }),
-        render: async function* ({ initialNodes }){
-          yield <Spinner />
-        
-          console.log(JSON.stringify({initialNodes}, null, 2))
+			return <p>{content}</p>;
+		},
+		tools: {
+			update_database_whiteboard: {
+				description:
+					"Update the whiteboard for the database modeling. it generates the current state of the database based on the conversation context ",
+				parameters: z.object({
+					initialNodes: z
+						.array(
+							z.object({
+								id: z
+									.string()
+									.describe(
+										"ID for the node, representing the table, eg: table_name",
+									),
+								position: z.object({
+									x: z
+										.number()
+										.describe("the position of the node in the x axis"),
+									y: z
+										.number()
+										.describe("the position of the node in the y axis"),
+								}),
+								data: z.object({
+									label: z.string().describe("the name of the table"),
+								}),
+							}),
+						)
+						.describe(
+							"an array of nodes definition for reactflow that'll represent the current state of the database",
+						),
+				}),
+				generate: async function* ({ initialNodes }) {
+					yield <Spinner />;
 
-          aiState.done([
-            ...aiState.get(),
-            {
-              role: "function",
-              name: "update_database_whiteboard",
-              // Content can be any string to provide context to the LLM in the rest of the conversation.
-              content: JSON.stringify(initialNodes),
-            }
-          ]);
+					console.log(JSON.stringify({ initialNodes }, null, 2));
 
-          // const initialNodes = [
-          //   { id: 'db_1', position: { x: 0, y: 0 }, data: { label: 'database 1' } },
-          //   { id: 'db_2', position: { x: 0, y: 100 }, data: { label: 'database 2' } },
-          //   { id: 'db_3', position: { x: 0, y: 200 }, data: { label: 'database 2' } },
-          // ];
-          // const initialEdges = [{ id: 'e1-2', source: 'db_1', target: 'db_2' }];
-          const initialEdges = [];
+					aiState.done([
+						...aiState.get(),
+						{
+							role: "tool",
+							name: "update_database_whiteboard",
+							// Content can be any string to provide context to the LLM in the rest of the conversation.
+							content: JSON.stringify(initialNodes),
+						},
+					]);
 
+					// const initialNodes = [
+					//   { id: 'db_1', position: { x: 0, y: 0 }, data: { label: 'database 1' } },
+					//   { id: 'db_2', position: { x: 0, y: 100 }, data: { label: 'database 2' } },
+					//   { id: 'db_3', position: { x: 0, y: 200 }, data: { label: 'database 2' } },
+					// ];
+					// const initialEdges = [{ id: 'e1-2', source: 'db_1', target: 'db_2' }];
+					const initialEdges = [];
 
-          return <DatabaseWhiteboard initialNodes={initialNodes} initialEdges={initialEdges} />
-        }
-      }
-    }
-  })
+					return (
+						<DatabaseWhiteboard
+							initialNodes={initialNodes}
+							initialEdges={initialEdges}
+						/>
+					);
+				},
+			},
+		},
+	});
 
-  return {
-    id: Date.now(),
-    display: ui
-  };
+	return {
+		id: Date.now(),
+		display: result.value,
+	};
 }
 
 // Define the initial state of the AI. It can be any JSON object.
@@ -157,11 +164,11 @@ const initialUIState = [];
 
 // AI is a provider you wrap your application with so you can access AI and UI state in your components.
 export const AI = createAI({
-  actions: {
-    submitUserMessage
-  },
-  // Each state can be any shape of object, but for chat applications
-  // it makes sense to have an array of messages. Or you may prefer something like { id: number, messages: Message[] }
-  initialUIState,
-  initialAIState
+	actions: {
+		submitUserMessage,
+	},
+	// Each state can be any shape of object, but for chat applications
+	// it makes sense to have an array of messages. Or you may prefer something like { id: number, messages: Message[] }
+	initialUIState,
+	initialAIState,
 });
