e2267bd061539253f32dd88c3425ce4d5ef609cc
Vik Borges <v1k@protonmail.com>
2024-06-16 13:58:49 +0800

    🚀

---
+++ frontend/src/app/auth/confirm/route.js
@@ -0,0 +1,40 @@
+
+import { NextResponse } from 'next/server'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type')
+  const next = searchParams.get('next') ?? '/'
+
+
+
+  const redirectTo = request.nextUrl.clone()
+  redirectTo.pathname = next
+  redirectTo.searchParams.delete('token_hash')
+  redirectTo.searchParams.delete('type')
+
+  console.log({token_hash})
+  console.log({type})
+
+  if (token_hash && type) {
+    const supabase = createClient()
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+    console.log({error})
+    if (!error) {
+      redirectTo.searchParams.delete('next')
+      return NextResponse.redirect(redirectTo)
+    }
+  }
+
+
+  // return the user to an error page with some instructions
+  redirectTo.pathname = '/error'
+  return NextResponse.redirect(redirectTo)
+}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 import { Chat } from "@/components/chat/main";
 import { AI } from "@/lib/chat/actions"; // Provider the gives access to AiState and UiState
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  
+  console.log({data})
+
   return ( 
     <AI>
       <Chat />
