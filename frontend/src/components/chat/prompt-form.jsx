540896b3d92252c1721c38c3fd380173138a48fc
Vik Borges <v1k@protonmail.com>
2024-07-08 21:06:34 +0800

    🚀

--- frontend/src/components/chat/prompt-form.jsx
+++ frontend/src/components/chat/prompt-form.jsx
@@ -10,6 +10,11 @@ import {
 	TooltipContent,
 	TooltipTrigger,
 } from "@/components/ui/tooltip";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
 
 import { useRouter } from "next/navigation";
 import { useActions, useUIState } from "ai/rsc";
@@ -17,6 +22,8 @@ import { nanoid } from "@/lib/utils";
 import { SpinnerMessage, UserMessage } from "@/components/chat/message";
 import { useEnterSubmit } from "@/hooks/use-enter-submit";
 import { generateId } from "ai";
+import Link from "next/link";
+import { TooltipArrow } from "@radix-ui/react-tooltip";
 
 export function PromptForm({ input, setInput }) {
 	const router = useRouter();
@@ -26,7 +33,7 @@ export function PromptForm({ input, setInput }) {
 	/**
 	 *
 	 */
-	const { submitUserMessage } = useActions();
+	const { submitUserMessage, testing } = useActions();
 
 	const [_, setMessages] = useUIState();
 
@@ -79,6 +86,36 @@ export function PromptForm({ input, setInput }) {
 			}}
 		>
 			<div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+				<Popover>
+					<PopoverTrigger asChild>
+						<Button
+							variant="outline"
+							size="icon"
+							className="absolute left-0 top-[14px] size-8 rounded-full bg-background p-0 sm:left-4"
+						>
+							<IconPlus />
+							<span className="sr-only">More options</span>
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent>
+						<div className="flex gap-2">
+							<div className="p-2 ">
+								<Link href="/">New Chat</Link>
+							</div>
+							<div className=" px-2">
+								<Button
+									variant="outline"
+									onClick={() => {
+										console.log("poop");
+									}}
+								>
+									Import Schema
+								</Button>
+							</div>
+						</div>
+					</PopoverContent>
+				</Popover>
+
 				<Textarea
 					ref={inputRef}
 					tabIndex={0}
