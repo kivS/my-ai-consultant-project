3cc94c99cc652d75f7e5405e19ebbc21d6e4e18a
Vik Borges <v1k@protonmail.com>
2024-08-02 22:55:16 +0800

    🚀

---
+++ backend/config/initializers/sentry.rb
@@ -0,0 +1,14 @@
+Sentry.init do |config|
+  config.dsn = 'https://c1c0c330a4061edb1c7941cdbfe95226@o481264.ingest.us.sentry.io/4507708135112704'
+
+  # get breadcrumbs from logs
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # enable tracing
+  # we recommend adjusting this value in production
+  config.traces_sample_rate = 1.0
+
+  # enable profiling
+  # this is relative to traces_sample_rate
+  config.profiles_sample_rate = 1.0
+end
