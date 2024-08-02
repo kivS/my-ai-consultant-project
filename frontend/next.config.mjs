94ea99a51f22c225c57f137a0c5ebc34ac814332
Vik Borges <v1k@protonmail.com>
2024-08-02 19:06:31 +0800

    🚀

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    trailingSlash: true,
+	output: "standalone",
+	trailingSlash: true,
+	compiler: {
+		removeConsole:
+			process.env.NODE_ENV === "production" ? { exclude: ["debug"] } : false,
+	},
 };
 
 export default nextConfig;
