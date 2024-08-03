94262ac59e5733d6b46e6cadbc317e4a8bb00343
Vik Borges <v1k@protonmail.com>
2024-08-03 08:32:28 +0800

    🚀

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.dsn = 'https://c1c0c330a4061edb1c7941cdbfe95226@o481264.ingest.us.sentry.io/4507708135112704'
 
-  config.enabled = Rails.env.production?
+  config.enabled_environments = %w[production]
 
   # get breadcrumbs from logs
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
